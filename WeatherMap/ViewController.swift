a5f5dd5807a729a85340cb1ec48a81fc35f55491
Roman Gornostayev <roman.gornostayev@yandex.ru>
2021-05-03 03:24:19 +0300

    Initial Commit

---
+++ WeatherMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherMap
+//
+//  Created by SalemMacPro on 3.5.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
